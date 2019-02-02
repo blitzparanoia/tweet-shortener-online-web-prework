ca1634a7350ec864bd9aa3a60af6a6f665939889
Irene Saez <bluemilk44@gmail.com>
2019-02-02 23:54:36 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -31,10 +31,11 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.length  > 140
+  if tweet.length > 140
     word_substituter(tweet)
   else
     tweet
   end
 end
 
+def shortened
