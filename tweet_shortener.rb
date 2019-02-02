fd8fbfd235194bf604517482410d130d2a004cff
Irene Saez <bluemilk44@gmail.com>
2019-02-02 23:57:07 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -40,6 +40,6 @@ end
 
 def shortened_tweet_truncator(tweet)
   if word_substituter(tweet).length > 140
-    word_substituter(tweet)[0...136]
+    word_substituter(tweet)[0...136] + "..."
   end
 end
